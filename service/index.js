de9fb44ebd7c65c35875d6f0aff0527f820e95c7
Sawyer Whipperman <sawyer57@byu.edu>
2026-03-11 11:30:38 -0600

    imports, middleware, router

--- service/index.js
+++ service/index.js
@@ -1,2 +1,14 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.static('public')); 
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
