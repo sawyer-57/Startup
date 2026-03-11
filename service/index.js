6ea12df60cc3aa4c790858a0240aa05ce37eb6c0
Sawyer Whipperman <sawyer57@byu.edu>
2026-03-11 12:01:38 -0600

    Helper functions for user

--- service/index.js
+++ service/index.js
@@ -74,3 +74,37 @@ apiRouter.post('/message', verifyAuth, (req, res) => {
   messages.push(message);
   res.send({ success: true });
 });
+
+
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
